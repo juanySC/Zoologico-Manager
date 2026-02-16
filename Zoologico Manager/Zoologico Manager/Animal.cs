955987b6da2004635995ba92dbe99634898b68f5
juanySC <48737486+juanySC@users.noreply.github.com>
2026-02-16 10:23:21 -0600

    Creacion de clase Animal

---
+++ Zoologico Manager/Zoologico Manager/Animal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Zoologico_Manager
+{
+    internal class Animal
+    {
+
+        //atributos
+        private string nombre;
+        private int edad;
+        private int salud;
+        private int hambre;
+
+        //constructor
+        public Animal(string nombre, int edad, int salud, int hambre)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.salud = salud; // salud inicial
+            this.hambre = hambre;  
+        }
+
+        //metodos
+        public virtual void EmitirSonido()
+        {
+            Console.WriteLine($"{nombre} hace un sonido.");
+        }
+
+        public virtual void Alimentar()
+        {
+
+            MessageBox.Show($"Alimentando a {nombre}...");
+            hambre -= 20; // reducir el hambre al alimentar
+            salud += 10; // aumentar la salud al alimentar
+
+        }
+
+        //getters and setters
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+    }
+}
---
+++ Zoologico Manager/Zoologico Manager/Form1.cs
@@ -0,0 +1,10 @@
+namespace Zoologico_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
